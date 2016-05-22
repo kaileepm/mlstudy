048ce7e2e4a8e3e43a41afc51f6527951eac6c4f
Kai Lee <kailee@nvidia.com>
2016-05-21 17:26:53 -0700

    Exercise 2: Normal equation solution.
    Got similar parameters with gradient descent approach.
    
    With normal equation
    
    theta: 340412.659574 110631.050279 -6649.474271
    Predicting...
    Prediction for (1650, 3): 293081.464335
    Prediction for (1000, 2): 211332.545336
    Prediction for (2000, 4): 333067.181129
    
    With gradient descent
    
    theta: 340412.659574 110630.818824 -6649.242816
    Predicting...
    Prediction for (1650, 3): 293081.514699
    Prediction for (1000, 2): 211332.480858
    Prediction for (2000, 4): 333067.433709

---
+++ 02-house-price/house_price_normal.m
@@ -0,0 +1,44 @@
+%% Exercise 2 using normal equation
+
+function house_price_normal()
+
+% Load data
+data = load('ex1data2.txt');
+
+y = data(:, 3);
+m = length(y);
+X = data(:, 1:2);
+
+% Feature scaling
+mu = mean(X);
+sigma = std(X);
+
+for i = 1:size(X, 2)
+	X_norm(:, i) = (X(:, i) - mu(i)) / sigma(i);
+end
+
+X = [ ones(m, 1) X_norm ];
+
+% Normal equation
+theta = (pinv(X' * X)) * (X') * y;
+fprintf('theta: %f %f %f\n', theta(1), theta(2), theta(3));
+
+% Prediction
+fprintf('Predicting...\n');
+MyPredictPrint(1650, 3, theta, mu, sigma);
+MyPredictPrint(1000, 2, theta, mu, sigma);
+MyPredictPrint(2000, 4, theta, mu, sigma);
+
+function price = MyPredict(x1, x2, theta, mu, sigma)
+	% normalize
+	sample = [ 1, (x1 - mu(1)) / sigma(1), (x2 - mu(2)) / sigma(2) ];
+	% calculate
+	price = sample * theta;
+endfunction
+
+function MyPredictPrint(x1, x2, theta, mu, sigma)
+	price = MyPredict(x1, x2, theta, mu, sigma);
+	fprintf('Prediction for (%d, %d): %f\n', x1, x2, price);
+endfunction
+
+endfunction
