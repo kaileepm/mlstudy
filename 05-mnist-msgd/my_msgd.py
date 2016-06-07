ac1e207d5597f481b439ae4b0c22eb482da86124
Kai Lee <kaileepm@gmail.com>
2016-06-07 10:58:08 -0700

    Move data to common area

--- 05-mnist-msgd/my_msgd.py
+++ 05-mnist-msgd/my_msgd.py
@@ -47,7 +47,7 @@ class MNISTData:
         #
 
         try:
-            fd = gzip.open('mnist.pkl.gz', 'rb')
+            fd = gzip.open('../data/mnist.pkl.gz', 'rb')
             train_set, valid_set, test_set = cPickle.load(fd)
             fd.close()
         except:
